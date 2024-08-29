c1f9126f7f6a91b703c168675e003d70f2ab2edc
guptaraghav22 <rg22052000@gmail.com>
2024-08-29 20:59:10 +0530

    fixed connection string

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,10 +2,13 @@ const { MongoClient } = require("mongodb");
 
 const connectDB = async () => {
   try {
-    const client = new MongoClient(process.env.mongoURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const client = new MongoClient(
+      "mongodb+srv://demoUser:3nFCaGYLkry17NDG@cluster0.z2rwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0PORT=2000",
+      {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      }
+    );
     await client.connect();
 
     return client;
