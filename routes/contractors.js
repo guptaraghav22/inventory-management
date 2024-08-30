ad17c74513c50af15fc7965024f310a3749d6cd2
guptaraghav22 <rg22052000@gmail.com>
2024-08-31 02:51:51 +0530

    contractor post and get api

--- routes/contractors.js
+++ routes/contractors.js
@@ -19,13 +19,56 @@ const storage = multer.diskStorage({
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
+const upload = multer({ storage });
 
-router.get("/", (req, res, next) => {
-  res.send("This works 1");
+router.get("/", async (req, res, next) => {
+  try {
+    let result = await contractorConnections.findOne({});
+
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(400).send({ message: "Error while getting contractor list" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
-router.post("/add", (req, res, next) => {
-  res.send("This works 2");
+router.post("/add", upload.single("image"), async (req, res, next) => {
+  try {
+    let { supplierName, product, contractorNumber, email, type, onTheWay } =
+      req.body;
+
+    let contractorId = uuidv4();
+    const imageUrl = req.file
+      ? `http://localhost:2000/${req.file.path.replace(/\\/g, "/")}`
+      : null;
+    let updatedContractor = {
+      supplierName,
+      product,
+      contractorNumber,
+      email,
+      type,
+      onTheWay,
+      contractorId,
+      // imageUrl,
+    };
+
+    let result = await contractorConnections.insertOne(updatedContractor);
+    console.log(updatedContractor);
+    if (result) {
+      res.status(200).send({ result });
+    } else {
+      res
+        .status(404)
+        .send({ message: "Some error occured while posting contractor" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Internal server" });
+  }
 });
 
 router.put("/:id", (req, res, next) => {
