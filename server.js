b3c1c1f889f6015b3a60e4c4520f408577ecb497
guptaraghav22 <rg22052000@gmail.com>
2024-08-31 01:19:39 +0530

    added cors

--- server.js
+++ server.js
@@ -1,39 +1,22 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const routes = require("./routes");
-// const router = express.Router();
-// const app = express();
-// dotenv.config();
-// const PORT = process.env.PORT;
-
-// app.use(express.json());
-// // JSON parser middleware
-// router.use(bodyParser.json());
-
-// // Form data parser middleware
-// router.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use("/uploads", express.static("uploads"));
-// app.use("/", routes);
-
-// app.listen(PORT, () => {
-//   console.log(`Server Running on PORT: ${PORT}`);
-// });
-
 const express = require("express");
 const dotenv = require("dotenv");
 const routes = require("./routes");
-const bodyParser = require("body-parser"); // Import body-parser
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
-
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-// JSON parser middleware
 app.use(bodyParser.json());
 
-// Form data parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static("uploads"));
