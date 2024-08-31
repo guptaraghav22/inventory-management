0dbbca46a852f7e982246999d70aacfc04179885
guptaraghav22 <rg22052000@gmail.com>
2024-08-31 16:28:31 +0530

    fixed mongodb

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,7 +3,7 @@ const { MongoClient } = require("mongodb");
 const connectDB = async () => {
   try {
     const client = new MongoClient(
-      "mongodb+srv://demoUser:3nFCaGYLkry17NDG@cluster0.z2rwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0PORT=2000",
+      "mongodb://demoUser:3nFCaGYLkry17NDG@cluster0.z2rwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0PORT=2000",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
